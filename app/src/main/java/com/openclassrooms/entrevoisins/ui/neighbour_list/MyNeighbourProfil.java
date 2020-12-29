318a39b8061acf8719bb9fa64f57ece99811f512
Alexandre <alexandre.beltz92@gmail.com>
2020-12-29 11:10:22 +0100

    V02
    Creation of myNeighbourProfil.java and XML attached.
    Add of setOnClickListener and Parcelable in Neighbour and MyNeighbourRecyclerViewAdapter.

---
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/MyNeighbourProfil.java
@@ -0,0 +1,33 @@
+package com.openclassrooms.entrevoisins.ui.neighbour_list;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.openclassrooms.entrevoisins.R;
+import com.openclassrooms.entrevoisins.model.Neighbour;
+
+import butterknife.OnClick;
+
+public class MyNeighbourProfil extends AppCompatActivity {
+
+    private ImageView mProfilPitcure;
+    private TextView mContactProfil;
+    private TextView mAboutProfil;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_neighbour_profil);
+
+        mProfilPitcure = (ImageView) findViewById(R.id.profil_picture);
+        mContactProfil = (TextView) findViewById(R.id.contact_profil);
+        mAboutProfil = (TextView) findViewById(R.id.about_profil);
+
+        Neighbour neighbour = getIntent().getExtras().getParcelable("myNeighbour");
+
+        mContactProfil.setText(neighbour.getName());
+    }
+
+}
--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/MyNeighbourRecyclerViewAdapter.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/MyNeighbourRecyclerViewAdapter.java
@@ -1,5 +1,7 @@
 package com.openclassrooms.entrevoisins.ui.neighbour_list;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -44,6 +46,15 @@ public class MyNeighbourRecyclerViewAdapter extends RecyclerView.Adapter<MyNeigh
                 .load(neighbour.getAvatarUrl())
                 .apply(RequestOptions.circleCropTransform())
                 .into(holder.mNeighbourAvatar);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent neighbourProfilIntent = new Intent(v.getContext(),MyNeighbourProfil.class);
+                neighbourProfilIntent.putExtra("myNeighbour", neighbour);
+                ((Activity) v.getContext()).startActivity(neighbourProfilIntent);
+
+            }
+        });
 
         holder.mDeleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
