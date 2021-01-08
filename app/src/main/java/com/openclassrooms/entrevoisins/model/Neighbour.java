eed6dbcc1e693c048d113b95a304ef87a9cd02c7
Alexandre <alexandre.beltz92@gmail.com>
2021-01-08 11:15:15 +0100

    V08
    Back to basic.
    
    - Mise en place du boolean dans Neighbour (non finis)
    - Suppression du bouton retour.
    - Ajout reseaux sociaux

--- app/src/main/java/com/openclassrooms/entrevoisins/model/Neighbour.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/model/Neighbour.java
@@ -28,6 +28,9 @@ public class Neighbour implements Parcelable {
     /** About me */
     private String aboutMe;
 
+    /** Is favorite */
+    private Boolean isFavorite;
+
     /**
      * Constructor
      * @param id
@@ -35,13 +38,14 @@ public class Neighbour implements Parcelable {
      * @param avatarUrl
      */
     public Neighbour(long id, String name, String avatarUrl, String address,
-                     String phoneNumber, String aboutMe) {
+                     String phoneNumber, String aboutMe, Boolean isFavorite) {
         this.id = id;
         this.name = name;
         this.avatarUrl = avatarUrl;
         this.address = address;
         this.phoneNumber = phoneNumber;
         this.aboutMe = aboutMe;
+        this.isFavorite = isFavorite;
     }
 
     protected Neighbour(Parcel in) {
@@ -51,6 +55,7 @@ public class Neighbour implements Parcelable {
         address = in.readString();
         phoneNumber = in.readString();
         aboutMe = in.readString();
+        /** isFavorite = in.readBooleanArray(true); */
     }
 
     public static final Creator<Neighbour> CREATOR = new Creator<Neighbour>() {
--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/MyNeighbourProfil.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/MyNeighbourProfil.java
@@ -1,12 +1,10 @@
 package com.openclassrooms.entrevoisins.ui.neighbour_list;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -25,50 +23,40 @@ import butterknife.OnClick;
 
 public class MyNeighbourProfil extends AppCompatActivity {
 
-    private ImageView mProfilPitcure;
-    private Button mButtonReturn;
-    private TextView mProfilName;
-    private TextView mProfilName2;
-    private TextView mLocalisationContact;
-    private TextView mNumberContact;
-    private TextView mSocialMedia;
-    private TextView mAboutInfo;
+        private ImageView mProfilPitcure;
+        private TextView mProfilName;
+        private TextView mProfilName2;
+        private TextView mLocalisationContact;
+        private TextView mNumberContact;
+        private TextView mSocialMedia;
+        private TextView mAboutInfo;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_neighbour_profil);
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().hide();
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_my_neighbour_profil);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+            mProfilPitcure = (ImageView) findViewById(R.id.profil_picture);
+            mProfilName = (TextView) findViewById(R.id.profil_name);
+            mProfilName2 = (TextView) findViewById(R.id.profil_name_2);
+            mLocalisationContact = (TextView) findViewById(R.id.localisation_contact);
+            mNumberContact = (TextView) findViewById(R.id.number_contact);
+            mSocialMedia = (TextView) findViewById(R.id.social_media);
+            mAboutInfo = (TextView) findViewById(R.id.about_info);
+
+            Neighbour neighbour = getIntent().getExtras().getParcelable("myNeighbour");
+
+            Glide.with(this)
+                    .load(neighbour.getAvatarUrl())
+                    .into(mProfilPitcure);
+
+            mProfilName.setText(neighbour.getName());
+            mProfilName2.setText(neighbour.getName());
+            mLocalisationContact.setText(neighbour.getAddress());
+            mNumberContact.setText(neighbour.getPhoneNumber());
+            mAboutInfo.setText(neighbour.getAboutMe());
+            mSocialMedia.setText("www.facebook/" + neighbour.getName());
         }
 
-        mProfilPitcure = (ImageView) findViewById(R.id.profil_picture);
-        Button mButtonReturn = (Button) findViewById(R.id.button_return);
-        mProfilName = (TextView) findViewById(R.id.profil_name);
-        mProfilName2 = (TextView) findViewById(R.id.profil_name_2);
-        mLocalisationContact = (TextView) findViewById(R.id.localisation_contact);
-        mNumberContact = (TextView) findViewById(R.id.number_contact);
-        mSocialMedia = (TextView) findViewById(R.id.social_media);
-        mAboutInfo = (TextView) findViewById(R.id.about_info);
-
-        Neighbour neighbour = getIntent().getExtras().getParcelable("myNeighbour");
-
-        Glide.with(this)
-                .load(neighbour.getAvatarUrl())
-                .into(mProfilPitcure);
-
-        mProfilName.setText(neighbour.getName());
-        mProfilName2.setText(neighbour.getName());
-        mLocalisationContact.setText(neighbour.getAddress());
-        mNumberContact.setText(neighbour.getPhoneNumber());
-        mAboutInfo.setText(neighbour.getAboutMe());
-
-        mButtonReturn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MyNeighbourProfil.this, ListNeighbourActivity.class));
-            }
-        });
     }
-
-}
